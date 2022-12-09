bf3d6a969306ce4189d8d7bdb1151bc69e630328
Florin Neacsu <florin.florin9908@gmail.com>
2022-12-09 11:22:28 +0200

    Keyboard Shortcuts

--- main.js
+++ main.js
@@ -177,5 +177,362 @@ const initApp = () => {
         currentValueElem.value = parseFloat(currentValueElem.value) * -1;
     });
 
+    document.addEventListener("keydown", e => {
+        if(e.code === 'Numpad0'){
+            const newInput = "0";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        
+        if(e.code === 'Numpad1'){
+            const newInput = "1";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        if(e.code === 'Numpad2'){
+            const newInput = "2";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        if(e.code === 'Numpad3'){
+            const newInput = "3";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        
+        if(e.code === 'Numpad4'){
+            const newInput = "4";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        
+        if(e.code === 'Numpad5'){
+            const newInput = "5";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        
+        if(e.code === 'Numpad6'){
+            const newInput = "6";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        
+        if(e.code === 'Numpad7'){
+            const newInput = "7";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        
+        if(e.code === 'Numpad8'){
+            const newInput = "8";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        
+        if(e.code === 'Numpad9'){
+            const newInput = "9";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+        
+        if(e.code === 'NumpadAdd'){
+            if (newNumberFlag) {
+                previousValueElem.textContent = "";
+                itemArray = [];
+            }
+            const newOperator = "+";
+            let currentVal = parseFloat(currentValueElem.value);
+            if (!itemArray.length) {
+                itemArray.push(currentVal, newOperator);
+                previousValueElem.textContent =
+                    `${currentVal} 
+                     ${newOperator}`;
+                return newNumberFlag = true;
+            }
+
+            if (itemArray.length) {
+                itemArray.push(currentVal);
+
+                const equationObj = {
+                    num1: parseFloat(itemArray[0]),
+                    num2: parseFloat(currentVal),
+                    op: itemArray[1]
+                }
+
+                equationArray.push(equationObj);
+                const newValue = operate(equationObj.num1, equationObj.num2, equationObj.op);
+                previousValueElem.textContent = `${newValue} ${newOperator}`;
+                itemArray = [newValue, newOperator];
+                newNumberFlag = true;
+                console.log(equationArray);
+            }
+        }
+        
+        if(e.code === 'NumpadSubtract'){
+            if (newNumberFlag) {
+                previousValueElem.textContent = "";
+                itemArray = [];
+            }
+            const newOperator = "-";
+            let currentVal = parseFloat(currentValueElem.value);
+            if (!itemArray.length) {
+                itemArray.push(currentVal, newOperator);
+                previousValueElem.textContent =
+                    `${currentVal} 
+                     ${newOperator}`;
+                return newNumberFlag = true;
+            }
+
+            if (itemArray.length) {
+                itemArray.push(currentVal);
+
+                const equationObj = {
+                    num1: parseFloat(itemArray[0]),
+                    num2: parseFloat(currentVal),
+                    op: itemArray[1]
+                }
+
+                equationArray.push(equationObj);
+                const newValue = operate(equationObj.num1, equationObj.num2, equationObj.op);
+                previousValueElem.textContent = `${newValue} ${newOperator}`;
+                itemArray = [newValue, newOperator];
+                newNumberFlag = true;
+                console.log(equationArray);
+            }
+        }
+
+        if(e.code === 'NumpadMultiply'){
+            if (newNumberFlag) {
+                previousValueElem.textContent = "";
+                itemArray = [];
+            }
+            const newOperator = "*";
+            let currentVal = parseFloat(currentValueElem.value);
+            if (!itemArray.length) {
+                itemArray.push(currentVal, newOperator);
+                previousValueElem.textContent =
+                    `${currentVal} 
+                     ${newOperator}`;
+                return newNumberFlag = true;
+            }
+
+            if (itemArray.length) {
+                itemArray.push(currentVal);
+
+                const equationObj = {
+                    num1: parseFloat(itemArray[0]),
+                    num2: parseFloat(currentVal),
+                    op: itemArray[1]
+                }
+
+                equationArray.push(equationObj);
+                const newValue = operate(equationObj.num1, equationObj.num2, equationObj.op);
+                previousValueElem.textContent = `${newValue} ${newOperator}`;
+                itemArray = [newValue, newOperator];
+                newNumberFlag = true;
+                console.log(equationArray);
+            }
+        }
+        
+        if(e.code === 'NumpadDivide'){
+            if (newNumberFlag) {
+                previousValueElem.textContent = "";
+                itemArray = [];
+            }
+            const newOperator = "/";
+            let currentVal = parseFloat(currentValueElem.value);
+            if (!itemArray.length) {
+                itemArray.push(currentVal, newOperator);
+                previousValueElem.textContent =
+                    `${currentVal} 
+                     ${newOperator}`;
+                return newNumberFlag = true;
+            }
+
+            if (itemArray.length) {
+                itemArray.push(currentVal);
+
+                const equationObj = {
+                    num1: parseFloat(itemArray[0]),
+                    num2: parseFloat(currentVal),
+                    op: itemArray[1]
+                }
+
+                equationArray.push(equationObj);
+                const newValue = operate(equationObj.num1, equationObj.num2, equationObj.op);
+                previousValueElem.textContent = `${newValue} ${newOperator}`;
+                itemArray = [newValue, newOperator];
+                newNumberFlag = true;
+                console.log(equationArray);
+            } 
+        }
+        if(e.code === 'NumpadEnter'){
+            const currentVal = currentValueElem.value;
+            let equationObj;
+
+            if (!itemArray.length && equationArray.length) {
+                const lastEquation = equationArray[equationArray.length - 1];
+                equationObj = {
+                    num1: parseFloat(currentVal),
+                    num2: lastEquation.num2,
+                    op: lastEquation.op
+                }
+            } else if (!itemArray.length) {
+                return currentVal;
+            } else {
+                itemArray.push(currentVal);
+                equationObj = {
+                    num1: parseFloat(itemArray[0]),
+                    num2: parseFloat(currentVal),
+                    op: itemArray[1]
+                }
+            }
+
+            equationArray.push(equationObj);
+            previousValueElem.textContent = `${equationObj['num1']} ${equationObj['op']} ${equationObj['num2']}`;
+            currentValueElem.value = operate(equationObj.num1, equationObj.num2, equationObj.op);
+            newNumberFlag = true;
+            itemArray = [];
+            console.log(equationArray);
+        } 
+        if(e.code === 'NumpadDecimal'){
+            const newInput = ".";
+            if (newNumberFlag) {
+                currentValueElem.value =
+                    newInput === '.'
+                        ? "0."
+                        : newInput;
+                newNumberFlag = false;
+            } else if (currentValueElem.value.includes('.') && newInput === '.') {
+                return;
+            } else {
+                currentValueElem.value = currentValueElem.value == 0 && currentValueElem.value.length == 1 && newInput !== '.'
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        }
+
+        if(e.code === 'Backspace'){
+            currentValueElem.value = currentValueElem.value.slice(0, -1);
+            if (!currentValueElem.value.length) currentValueElem.value = 0;
+        }
+        if(e.code === 'Delete'){
+            currentValueElem.value = 0;
+            previousValueElem.textContent = '';
+        }
+    })
+
 }
+
 document.addEventListener("DOMContentLoaded", initApp);
